6dfe3753855ddcc7c9f6df5c5fa6e9293c5bb700
Saurabh Kumar <153168387+SaurabhKumar440@users.noreply.github.com>
2025-11-08 19:49:52 +0000

    datatype

---
+++ 01_basics/02_datatype.js
@@ -0,0 +1,21 @@
+"use script"; //treat all js code as newer version
+
+//alert("hello world"); //we are using node js not browser
+
+console.log("hello world");
+
+let name = "saurabh";
+let age = 21;
+let isApproved = false;
+
+console.log(typeof name);
+console.log(typeof age);
+console.log(typeof isApproved);
+//number
+//string
+//boolean
+//undefined
+//null
+//symbol
+
+//object

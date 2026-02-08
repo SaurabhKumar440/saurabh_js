d44a72b5d1131b91c3c1cf2410b24736d632298b
Saurabh Kumar <153168387+SaurabhKumar440@users.noreply.github.com>
2026-02-08 08:46:54 +0000

    conops

---
+++ 01_basics/conversionOperation.js
@@ -0,0 +1,31 @@
+let score="33"
+//console.log(typeof score);
+//console.log(typeof(score));
+//let valueInNumber=Number(score);
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber);
+
+//"33" => 33
+//"33abc" => NaN
+//true => 1
+//false => 0
+//null => 0
+//undefined => NaN
+
+//let isLoggedIn=;
+//let booleanIsLoggedIn=Boolean(isLoggedIn);
+//console.log(booleanIsLoggedIn);
+//undefined => false
+//null => false
+//0 => false
+//NaN => false
+//"" => false
+//" " => true
+//[] => true
+//{} => true
+
+let someNumber=33;
+let stringNumber=String(someNumber);
+console.log(stringNumber);
+console.log(typeof stringNumber);
+//33 => "33"
